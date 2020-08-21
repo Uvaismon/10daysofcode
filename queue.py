55798bef33c7482c46a41538101657dd1920b2d0
Uvaismon <uvaisullas@gmail.com>
2020-08-21 11:06:52 +0530

    Problem 4 v2

--- queue.py
+++ queue.py
@@ -1,12 +1,28 @@
-from collections import deque
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+class Queue:
+    def __init__(self):
+        self.stack1 = []
+
+    def enqueue(self, x):
+       self.stack1.append(x)
+
+    def dequeue(self):
+        stack2 = self.stack1[1:]
+        self.stack1.clear()
+        self.stack1 = stack2[::]
+
+    def peek(self):
+        print(self.stack1[0])
 
 
-q = deque()
-for i in range(int(input())):
+q = Queue()
+
+for _ in range(int(input())):
     o = list(map(int, input().split()))
     if o[0] == 1:
-        q.append(o[1])
+        q.enqueue(o[1])
     if o[0] == 2:
-        q.popleft()
+        q.dequeue()
     if o[0] == 3:
-        print(q[0])
+        q.peek()
