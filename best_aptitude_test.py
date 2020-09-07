fb88acebcb28240c0415d3ef4b41a302da15cc48
Uvaismon <uvaisullas@gmail.com>
2020-09-07 13:11:55 +0530

    Improved best_aptitude_test

--- best_aptitude_test.py
+++ best_aptitude_test.py
@@ -1,11 +1,11 @@
 from functools import cmp_to_key
-from statistics import variance
+from statistics import mean, variance
 
 def comparator(t1, t2):
     if t1.score > t2.score:
-        return -1
-    elif t1.score < t2.score:
         return 1
+    elif t1.score < t2.score:
+        return -1
     else:
         return 0
 
@@ -14,31 +14,26 @@ class test_cases:
     def __init__(self, gpa, test_number):
         self.gpa = gpa
         self.test_number = test_number
-        self.grades = []
+        self.grades = list(map(float, input().split()))
         self.score = 0
-        self.var = 0
-        self.read()
-        self.scoring()
-        if self.var == 1:
-            self.score = 0
+        if variance(self.grades) == 0 and variance(self.gpa) != 0:
+            self.score = float('inf')
+        else:
+            self.scoring()
 
-    def read(self):
-        inp = list(map(float, input().split()))
-        if variance(inp) == 0:
-            self.var = 1
-        self.grades = [(i, inp.index(i)+1) for i in inp]
-    
     def scoring(self):
-        self.grades.sort()
         for i in range(len(self.grades)):
-            if self.grades[i][1] == self.gpa[i][1]:
-                self.score += 1
+            self.grades[i] = (self.grades[i] - mean(self.grades)) / (max(self.grades) - min(self.grades))
+        diff = []
+        for i in range(len(self.grades)):
+            diff.append((self.gpa[i] - self.grades[i]) ** 2)
+        self.score = mean(diff)
     
-
 for _ in range(int(input())):
     intake = int(input())
     gpa = list(map(float, input().split()))
-    gpa = [(i, gpa.index(i)+1) for i in gpa]
-    tests = [test_cases(gpa, i+1) for i in range(5)]
-    tests = sorted(tests, key=cmp_to_key(comparator))
-    print(tests[0].test_number)
+    for i in range(intake):
+        gpa[i] = (gpa[i] - mean(gpa)) / (max(gpa) - min(gpa))
+    test = [test_cases(gpa, i+1) for i in range(5)]
+    test = sorted(test, key=cmp_to_key(comparator))
+    print(test[0].test_number)
